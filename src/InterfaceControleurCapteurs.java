207a8b813f85764b68c71fedce143d76c4a0e9c8
Maxador <mongeaumax@gmail.com>
2013-11-22 12:04:11 -0500

    Création interface controleur et structure de la classe

--- src/InterfaceControleurCapteurs.java
+++ src/InterfaceControleurCapteurs.java
@@ -1,4 +1,93 @@
 
 public class InterfaceControleurCapteurs {
-
+	
+	/**
+	 * 
+	 * @param temperature
+	 */
+	public void ChangerTemperature(int temperature) {
+		
+	}
+	
+	/**
+	 * 
+	 * @param niveauEau
+	 */
+	public void ChangerNiveauEau(int niveauEau) {
+		
+	}
+	
+	/**
+	 * 
+	 * @param vitesse
+	 */
+	public void ChangerVitesseRotation(int vitesse) {
+		
+	}
+	
+	/**
+	 * 
+	 * @param cadence
+	 */
+	public void ChangerCadence(int cadence) {
+		
+	}
+	
+	/**
+	 * 
+	 * @param dureeMinutes
+	 */
+	public void ChangerDureeLavage(int dureeMinutes) {
+		
+	}
+	
+	/**
+	 * 
+	 */
+	public void AjouterSavon() {
+		
+	}
+	
+	/**
+	 * 
+	 */
+	public void AjouterAssouplisseur() {
+		
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public int ObtenirTemperature() {
+		return 0;
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public int ObtenirCadence() {
+		return 0;
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public int ObtenirVitesse() {
+		return 0;
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public int ObtenirNiveauEau() {
+		return 0;
+	}
+	
+	public void EvacuerEauUsee() {
+		
+	}
 }
